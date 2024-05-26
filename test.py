01aee217c86ee1292757bc753a98794d931e6936
Babu Nair <babunairofficial@gmail.com>
2024-05-26 11:14:54 +0530

    Datatypes in python

---
+++ test.py
@@ -0,0 +1,18 @@
+#datatypes
+name = "babu"
+age = 33
+marks = float(98)
+number = "16"
+jersey_no = int(number)
+
+print(type(name))
+print(type(age))
+
+print(type(name) == str)
+
+print(isinstance(name,str))
+
+print(isinstance(marks, float))
+
+print(isinstance(number, int))
+print(isinstance(jersey_no, int))
