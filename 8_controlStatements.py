10cbb35e96f17b18916435271e915da22e50561a
Babu Nair <babunairofficial@gmail.com>
2024-09-10 00:40:51 +0530

    control statements in python

---
+++ 8_controlStatements.py
@@ -0,0 +1,10 @@
+#control statements
+condition = True
+if condition == True:
+  print("The condition")
+  print("was true")
+else:
+  print("The condition")
+  print("was false")
+
+print("outside if")
