bf367390212d76474b563002e171fb6014c5eb24
Babu Nair <babunairofficial@gmail.com>
2024-09-09 21:25:32 +0530

    booleans in python

---
+++ 4_booleans.py
@@ -0,0 +1,15 @@
+#booleans
+done = True 
+not_done = False
+
+print(type(done) == bool)
+
+if done: #yes should print
+  print("yes")
+else:
+  print("no")
+
+if not_done: # no should print
+  print("yes")
+else:
+  print ("no")
