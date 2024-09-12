25e936c3ac2df1d8cc0bf540b6f1cbe8bfe8ac55
Babu Nair <babunairofficial@gmail.com>
2024-09-12 23:22:11 +0530

    lambda functions in python

---
+++ 5e_lambdaFunctions.py
@@ -0,0 +1,14 @@
+#Lambda Functions
+
+#also called anonymous function 
+#they have no name and only have 1 expression as their body
+#defined using lamda keyword
+
+# argument : expression
+lambda num : num * 2
+
+multiply = lambda a, b : a * b
+
+print(multiply(2,4))
+
+#lambda functions work better when combined with other functions eg map, filter and reduce
