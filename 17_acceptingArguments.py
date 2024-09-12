8ba9ac50c4abb686e2f67d1a12e70d568149d64b
Babu Nair <babunairofficial@gmail.com>
2024-09-12 23:14:02 +0530

    accepting arguments using command line in python

---
+++ 17_acceptingArguments.py
@@ -0,0 +1,21 @@
+#Accepting Arguments
+
+import sys
+import argparse
+
+
+print(sys.argv) #print the argument through command line
+
+name = sys.argv[1]
+
+print ("Hello" + name)
+
+parser = argparse.ArgumentParser(
+    description='This program prints the name of my dogs'
+)
+
+parser.add_argument('-c', '--color', metavar='color', required=True, choices={'red','blue'}, help='the color to search for')
+
+args = parser.parse_args()
+
+print(args.color)
