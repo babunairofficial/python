a8f7e88d7541f9ab9b26d8b54e5ff45d433d62d1
Babu Nair <babunairofficial@gmail.com>
2024-09-16 16:08:25 +0530

    assert keyword in python

--- 24_constructor.py
+++ 24_constructor.py
@@ -2,9 +2,13 @@
 #used for instantiating an object
 
 class Item:
-    def __init__(self, name, price, quantity=0): #constructor
+    def __init__(self, name: str, price: float, quantity=0): #constructor
         
         #body of the constructor
+
+        #assertion 
+        assert price >= 0, f"Price {price} is not greater than or equal to 0"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to 0"
         self.name = name
         self.price = price
         self.quantity = quantity
