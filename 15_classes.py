a1156fdf7963219fe40e6d427b8d90f61d3d899f
Babu Nair <babunairofficial@gmail.com>
2024-09-16 11:08:23 +0530

    instance, attributes, method in python

--- 15_classes.py
+++ 15_classes.py
@@ -66,3 +66,28 @@ dora = Dog('Dora', 2)
 print(handsome > dora)
  
 #there are several other special functions or methods for operator overloading
+
+#Creating instances in class
+class Item:
+    def calculate_total_price(self, x, y): #method
+        return x * y
+    
+
+item1 = Item() #instance
+
+#attributes
+item1.name = "Phone" 
+item1.price = 100
+item1.quantity = 5
+
+print(type(item1)) #this line prints a new datatype created different from the regular ones
+print(type(item1.name))
+print(type(item1.price))
+print(type(item1.quantity))
+print(item1.calculate_total_price(item1.price, item1.quantity))
+
+item2 = Item() 
+item2.name = "Laptop"
+item2.price = 50000
+item2.quantity = 2
+print(item2.calculate_total_price(item2.price, item2.quantity))
